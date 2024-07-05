d67f2f1cd5b24b29d113ac9a0a912714ac5829f6
ajay <ajay.staplelogic@gmail.com>
2024-07-05 12:31:11 +0530

    landlord and manager info added

--- src/admin/routes/properties.route.mjs
+++ src/admin/routes/properties.route.mjs
@@ -2,10 +2,6 @@ import express from 'express'
 const router = express.Router();
 import {properties} from "../controllers/properties.controller.mjs"
 
-
-
-
-
 router.get('/properties' , properties )
 
 export default router;
--- src/admin/services/properties.service.mjs
+++ src/admin/services/properties.service.mjs
@@ -1,17 +1,67 @@
 import { Property } from "../../user/models/property.model.mjs";
 
 async function getPropertiesList() {
-     const data = await Property.find();
-  
-    return {
-      data: data,
-      message: `successfully fetched  list`,
-      status: true,
-      statusCode: 201,
-    };
+  const data = await Property.aggregate([{
+    $lookup: {
+      from: "users",
+      let: { renter_ID: { $toObjectId: "$landlord_id" } },
+      pipeline: [
+        {
+          $match: {
+            $expr: { $eq: ["$_id", "$$renter_ID"] }
+          }
+        },
+        {
+          $project: {
+            _id: 1,
+            fullName: 1, // Include fullName field from users collection
+            countryCode: 1,
+            phone: 1
+
+          }
+        }
+      ],
+      as: "landlord_info",
+
+    }
+  }, {
+
+    $lookup: {
+      from: "users",
+      let: { renter_ID: { $toObjectId: "$property_manager_id" } },
+      pipeline: [
+        {
+          $match: {
+            $expr: { $eq: ["$_id", "$$renter_ID"] }
+          }
+        },
+        {
+          $project: {
+            _id: 1,
+            fullName: 1, // Include fullName field from users collection
+            countryCode: 1,
+            phone: 1,
+            picture: 1
+
+          }
+        }
+      ],
+      as: "propertyManager_info",
+
+    }
+
   }
+  ])
+
+  return {
+    data: data,
+    message: `successfully fetched  list`,
+    status: true,
+    statusCode: 201,
+  };
+}
 
 
 export {
-    getPropertiesList
+  getPropertiesList
 }
