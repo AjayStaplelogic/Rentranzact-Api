b4be234f4c3520e05178bd4dddc26ab9c988c1c0
ajay <ajay.staplelogic@gmail.com>
2024-07-10 15:35:41 +0530

    made webhooks dynamic

--- src/user/controllers/stripes.controller.mjs
+++ src/user/controllers/stripes.controller.mjs
@@ -2,9 +2,9 @@ import { sendResponse } from "../helpers/sendResponse.mjs";
 import { addStripeTransaction } from "../services/strips.service.mjs";
 async function stripe(req, res) {
 
-    console.log(body, "=========boddddyyyyy")
     const { body } = req;
 
+    console.log(body, "=========boddddyyyyy")
     const data = await addStripeTransaction(body);
 
     sendResponse(res, data.data, data.message, data.status, data.statusCode);
