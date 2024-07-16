7ff807e05190ed1c4aee19b5d486dda231867d86
ajay <ajay.staplelogic@gmail.com>
2024-07-16 15:52:51 +0530

    made property rented

--- src/user/services/strips.service.mjs
+++ src/user/services/strips.service.mjs
@@ -1,5 +1,8 @@
 import Stripe from "stripe";
 import { Transaction } from "../models/transactions.model.mjs";
+import { Property } from "../models/property.model.mjs";
+import { RentType } from "../enums/property.enums.mjs";
+import moment from "moment";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
@@ -11,9 +14,54 @@ async function addStripeTransaction(body) {
 
     const { amount, status, created, id } = body.data.object;
 
+
+    const propertyDetails = await Property.findById(propertyID);
+
+    if (propertyDetails.rentType === RentType.MONTHLY) {
+
+        // Convert timestamp to a Moment.js object
+        const originalDate = moment.unix(created);
+
+        // Add one year to the original date
+        const oneYearLater = originalDate.add(1, 'months');
+
+        // Get the Unix timestamp of one year later
+        const timestampOneYearLater = oneYearLater.unix();
+
+        const updateProperty = await Property.findByIdAndUpdate(propertyID, { rented: true, renterID: userID, rent_period_start: created, rent_period_end: timestampOneYearLater })
+
+
+    } else if (propertyDetails.rentType === RentType.QUATERLY) {
+        // Convert timestamp to a Moment.js object
+        const originalDate = moment.unix(created);
+
+        // Add one year to the original date
+        const oneYearLater = originalDate.add(3, 'months');
+
+        // Get the Unix timestamp of one year later
+        const timestampOneYearLater = oneYearLater.unix();
+        const updateProperty = await Property.findByIdAndUpdate(propertyID, { rented: true, renterID: userID, rent_period_start: created, rent_period_end: timestampOneYearLater })
+
+
+    } else if (propertyDetails.rentType === RentType.YEARLY) {
+        // Convert timestamp to a Moment.js object
+        const originalDate = moment.unix(created);
+
+        // Add one year to the original date
+        const oneYearLater = originalDate.add(1, 'yearly');
+
+        // Get the Unix timestamp of one year later
+        const timestampOneYearLater = oneYearLater.unix();
+        const updateProperty = await Property.findByIdAndUpdate(propertyID, { rented: true, renterID: userID, rent_period_start: created, rent_period_end: timestampOneYearLater })
+
+    }
+
+
     const data = new Transaction({ renterID: userID, propertyID: propertyID, amount: amount, status: status, date: created, intentID: id })
 
     data.save()
+
+
 
     return {
 
