008f947833a723a61a03776bac973dcd3da09446
ajay <ajay.staplelogic@gmail.com>
2024-07-17 14:55:47 +0530

    added my-renters api

--- src/user/routes/rentingHistory.route.mjs
+++ src/user/routes/rentingHistory.route.mjs
@@ -5,7 +5,7 @@ import { myRenters } from '../controllers/myrenter.controller.mjs';
 import authorizer from '../middleware/authorizer.middleware.mjs';
 
 
-router.post('/my-renters' , authorizer([UserRoles.LANDLORD])  , myRenters);
+router.get('/my-renters' , authorizer([UserRoles.LANDLORD])  , myRenters);
 
 export default router;
 
