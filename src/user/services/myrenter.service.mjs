134b16ea4949129c1d65ef8e31f7be2a868a935c
ajay <ajay.staplelogic@gmail.com>
2024-07-17 14:59:43 +0530

    added my-renters api

--- src/user/services/myrenter.service.mjs
+++ src/user/services/myrenter.service.mjs
@@ -3,6 +3,16 @@ import { RentingHistory } from "../models/rentingHistory.model.mjs";
 
 async function myRentersService(id) {
 
+
+    console.log(id, "----")
+
+    const data2 = await RentingHistory.find({landlordID : id})
+
+
+    console.log(data2 , "====data2 ")
+
+
+
     const data = await RentingHistory.aggregate([{
         $match: {
             landlordID: id
