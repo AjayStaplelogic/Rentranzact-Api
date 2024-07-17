e442632d5eecb4cb815e2a397aabebbe9084d35a
ajay <ajay.staplelogic@gmail.com>
2024-07-17 14:52:44 +0530

    added my-renters api

--- src/user/routes/rentingHistory.route.mjs
+++ src/user/routes/rentingHistory.route.mjs
@@ -1,12 +1,12 @@
 import express from 'express'
 const router = express.Router();
-import {authorizer} from "../middleware/authorizer.middleware.mjs"
 import {UserRoles} from "../enums/role.enums.mjs"
 import { myRenters } from '../controllers/myrenter.controller.mjs';
+import authorizer from '../middleware/authorizer.middleware.mjs';
+
+s
 
 router.post('/my-renters' , authorizer([UserRoles.LANDLORD])  , myRenters);
-
-
 
 export default router;
 
