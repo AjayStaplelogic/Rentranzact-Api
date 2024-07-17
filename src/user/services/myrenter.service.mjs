c16652223764752b5a61260c3f87e56d889743e6
ajay <ajay.staplelogic@gmail.com>
2024-07-17 15:12:11 +0530

    added property details

--- src/user/services/myrenter.service.mjs
+++ src/user/services/myrenter.service.mjs
@@ -6,17 +6,32 @@ async function myRentersService(id) {
 
     console.log(id, "----")
 
-    const data2 = await RentingHistory.find({landlordID : id})
+    const data2 = await RentingHistory.find({ landlordID: id })
 
 
-    console.log(data2 , "====data2 ")
+    console.log(data2, "====data2 ")
 
 
 
     const data = await RentingHistory.aggregate([{
         $match: {
             landlordID: id
+        },
+
+        $lookup: {
+            from: "properties",
+            let: { propertyID: { $toObjectId: "$propertyID" } }, // Convert propertyID to ObjectId
+            pipeline: [
+                {
+                    $match: {
+                        $expr: { $eq: ["$_id", "$$propertyID"] }, // Match ObjectId type
+                    },
+                },
+                { $project: { propertyName: 1, address: 1 } }, // Project only the images array from properties
+            ],
+            as: "propertyDetails",
         }
+
     }])
 
 
