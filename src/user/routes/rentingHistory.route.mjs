e6345f15f5c61d3a3a4532f2264c8b91adf65b82
ajay <ajay.staplelogic@gmail.com>
2024-07-17 14:54:06 +0530

    added my-renters api

--- src/user/routes/rentingHistory.route.mjs
+++ src/user/routes/rentingHistory.route.mjs
@@ -4,7 +4,6 @@ import {UserRoles} from "../enums/role.enums.mjs"
 import { myRenters } from '../controllers/myrenter.controller.mjs';
 import authorizer from '../middleware/authorizer.middleware.mjs';
 
-s
 
 router.post('/my-renters' , authorizer([UserRoles.LANDLORD])  , myRenters);
 
