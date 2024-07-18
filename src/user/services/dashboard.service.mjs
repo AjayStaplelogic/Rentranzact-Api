5e419f38f12e130bd52159707dd6ba7930853d82
ajay <ajay.staplelogic@gmail.com>
2024-07-18 14:51:33 +0530

    add maintenance count to dashboard

--- src/user/services/dashboard.service.mjs
+++ src/user/services/dashboard.service.mjs
@@ -1,19 +1,16 @@
 import { Inspection } from "../models/inspection.model.mjs";
+import { Maintenance } from "../models/maintenance.model.mjs";
 import { Property } from "../models/property.model.mjs";
-import { User } from "../models/user.model.mjs";
 
 async function getDashboardStats(user) {
-
-    console.log(user, "00000userrrrr")
-
+ 
     const rented = await Property.find({ landlord_id: user._id, rented: true }).countDocuments();
 
     const vacant = await Property.find({ landlord_id: user._id, rented: false }).countDocuments();
 
-    const maintenance = 0;
+    const maintenance = await Maintenance.find({landlordID : user._id , status : true });
 
     const total = await Property.find({ landlord_id: user._id }).countDocuments()
-
 
     const mostRecentInspection = await Inspection.aggregate([
         {
