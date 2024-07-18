750fb585aceeeba329b47e80114e03ee1f762499
ajay <ajay.staplelogic@gmail.com>
2024-07-18 18:52:16 +0530

    added data of finance

--- src/user/services/transaction.service.mjs
+++ src/user/services/transaction.service.mjs
@@ -4,7 +4,29 @@ import { Transaction } from "../models/transactions.model.mjs";
 async function getMyTransaction(userID, role) {
     if (role === UserRoles.RENTER) {
 
-        const data = await Transaction.find({ renterID: userID })
+        // const data = await Transaction.find({ renterID: userID })
+
+        const data = await Transaction.aggregate([
+            {
+                $match: {
+                    renterID: userID
+                },
+            }, {
+                $lookup: {
+                    from: "users",
+                    let: { userID: { $toObjectId: "$landlordID" } }, // Convert propertyID to ObjectId
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: { $eq: ["$_id", "$$userID"] }, // Match ObjectId type
+                            },
+                        },
+                        { $project: { picture: 1 } }, // Project only the images array from properties
+                    ],
+                    as: "landlordDetials",
+                }
+            }
+        ])
 
         return {
             data: data,
