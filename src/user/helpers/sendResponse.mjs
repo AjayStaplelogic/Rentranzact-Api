5f47c74e9bb8bab8f4155d895d9b76b837a94748
ajay <ajay.staplelogic@gmail.com>
2024-07-18 17:04:37 +0530

    added  log

--- src/user/helpers/sendResponse.mjs
+++ src/user/helpers/sendResponse.mjs
@@ -1,43 +1,43 @@
-// function sendResponse(res, data, message, status, code , accessToken, additionalData) {
-//   console.log(additionalData)
-// let responseObject;
-//   if (accessToken) {
-//     let additionalResponse;
-//     if(additionalData) {
-//       additionalResponse = additionalData
-//     } else {additionalResponse = []}
-//     responseObject = { data, message, status, accessToken, additionalData : additionalResponse  };
-
-//   } else {
-//      responseObject = { data, message, status };
-
-//   }
-
-
-//   res.status(code).json(responseObject);
-// }
-// export { sendResponse };
-
-function sendResponse(res, data, message, status, code, accessToken, additionalData) {
-  console.log(additionalData, "===additionl")
-  let responseObject;
-  let additionalResponse;
+function sendResponse(res, data, message, status, code , accessToken, additionalData) {
+  console.log(additionalData)
+let responseObject;
   if (accessToken) {
-
-
-    if (additionalData) {
-
+    let additionalResponse;
+    if(additionalData) {
       additionalResponse = additionalData
-    } else { additionalResponse = [] }
-    responseObject = { data, message, status, accessToken, additionalData: additionalResponse };
+    } else {additionalResponse = []}
+    responseObject = { data, message, status, accessToken, additionalData : additionalResponse  };
 
   } else {
-    if (additionalData) {
-
-      additionalResponse = additionalData
-    } else { additionalResponse = [] }
-    responseObject = { data, message, status, accessToken, additionalData: additionalResponse };
+     responseObject = { data, message, status };
 
   }
+
+
+  res.status(code).json(responseObject);
 }
 export { sendResponse };
+
+// function sendResponse(res, data, message, status, code, accessToken, additionalData) {
+//   console.log(additionalData, "===additionl")
+//   let responseObject;
+//   let additionalResponse;
+//   if (accessToken) {
+
+
+//     if (additionalData) {
+
+//       additionalResponse = additionalData
+//     } else { additionalResponse = [] }
+//     responseObject = { data, message, status, accessToken, additionalData: additionalResponse };
+
+//   } else {
+//     if (additionalData) {
+
+//       additionalResponse = additionalData
+//     } else { additionalResponse = [] }
+//     responseObject = { data, message, status, accessToken, additionalData: additionalResponse };
+
+//   }
+// }
+// export { sendResponse };
