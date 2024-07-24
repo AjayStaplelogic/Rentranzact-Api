c88615aea93da39b6005ead5ca3079501621e6c4
ajay <ajay.staplelogic@gmail.com>
2024-07-24 16:09:11 +0530

    added wallet logic

--- src/user/controllers/stripe.controller.mjs
+++ src/user/controllers/stripe.controller.mjs
@@ -1,14 +1,28 @@
 import { sendResponse } from "../helpers/sendResponse.mjs";
-import {payRentService} from "../services/stripe.service.mjs"
+import { payRentService } from "../services/stripe.service.mjs"
 
 async function payRent(req, res) {
   const { body } = req;
-  
-  const userID = req.user.data._id;
 
-  const data = await payRentService(body, userID);
 
-  sendResponse(res, data.data, data.message, data.status, data.statusCode);
+  if (body.wallet) {
+
+    const userID = req.user.data._id;
+    const data = await addToWallet(body, userID);
+    sendResponse(res, data.data, data.message, data.status, data.statusCode);
+
+
+  } else {
+    const userID = req.user.data._id;
+    const data = await payRentService(body, userID);
+    sendResponse(res, data.data, data.message, data.status, data.statusCode);
+  }
+
+
+
+
+
+
 }
 
 export { payRent };
--- src/user/services/stripe.service.mjs
+++ src/user/services/stripe.service.mjs
@@ -3,8 +3,8 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 async function payRentService(body, userID) {
-    
-    const { amount , propertyID } = body;
+
+    const { amount, propertyID, wallet } = body;
 
     const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
@@ -14,7 +14,37 @@ async function payRentService(body, userID) {
         },
         metadata: {
             propertyID: propertyID,
-            userID : userID
+            userID: userID,
+            wallet : wallet
+        }
+    });
+
+    return {
+        data: { client_secret: paymentIntent.client_secret },
+        message: "client secret created successfully",
+        status: true,
+        statusCode: 200,
+    };
+
+
+
+
+}
+
+
+async function addToWallet(body, userID) {
+
+    const { amount, wallet  } = body;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'ngn',
+        automatic_payment_methods: {
+            enabled: true,
+        },
+        metadata: {
+            wallet : wallet,
+            userID: userID
         }
     });
 
@@ -26,4 +56,5 @@ async function payRentService(body, userID) {
     };
 }
 
-export { payRentService };
+
+export { payRentService, addToWallet };
