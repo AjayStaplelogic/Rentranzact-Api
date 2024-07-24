47e70d85439cb23a60441460d33076bf866a3b83
ajay <ajay.staplelogic@gmail.com>
2024-07-24 16:09:42 +0530

    added wallet logic

--- src/user/controllers/stripe.controller.mjs
+++ src/user/controllers/stripe.controller.mjs
@@ -1,5 +1,5 @@
 import { sendResponse } from "../helpers/sendResponse.mjs";
-import { payRentService } from "../services/stripe.service.mjs"
+import { payRentService , addToWallet} from "../services/stripe.service.mjs"
 
 async function payRent(req, res) {
   const { body } = req;
