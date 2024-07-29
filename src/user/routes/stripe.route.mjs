7de9491dcee1b91926418dd766fe2109b638c683
ajay <ajay.staplelogic@gmail.com>
2024-07-29 18:19:07 +0530

    logging

--- src/user/routes/stripe.route.mjs
+++ src/user/routes/stripe.route.mjs
@@ -4,7 +4,7 @@ import { payRent } from '../controllers/stripe.controller.mjs';
 import { UserRoles } from '../enums/role.enums.mjs';
 import authorizer from "../middleware/authorizer.middleware.mjs";
 
-router.post('/payRent', authorizer([UserRoles.RENTER]), payRent);
+router.post('/payRent', authorizer([UserRoles.RENTER, UserRoles.LANDLORD]), payRent);
 
 export default router;
 
