68a3c4fdf65784cf674cdfc9d1d591aec692f9bf
ajay <ajay.staplelogic@gmail.com>
2024-08-09 12:22:31 +0530

    test

--- src/user/services/user.service.mjs
+++ src/user/services/user.service.mjs
@@ -222,7 +222,7 @@ async function socialSignup(body) {
         socialPlatform: socialPlatform,
       });
 
-      await User.findByIdAndUpdate(user.__id, {fcmToken : fcmToken})
+      await User.findByIdAndUpdate(user._id, {fcmToken : fcmToken})
 
       // console.log(user, "0-----user");
 
