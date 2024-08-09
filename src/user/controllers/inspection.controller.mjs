bb397d69c62a6d2abcaf5fdb92260a0d7d6f392b
vijay <vijay@staplelogic.in>
2024-08-09 16:56:02 +0530

    created inspections API

--- src/user/controllers/inspection.controller.mjs
+++ src/user/controllers/inspection.controller.mjs
@@ -10,6 +10,8 @@ import {
   searchInspectionService
 } from "../services/inspection.service.mjs";
 import { Inspection } from "../models/inspection.model.mjs";
+import { UserRoles } from "../enums/role.enums.mjs";
+
 
 async function getAvailableDates(req, res) {
   const { id } = req.params;
@@ -81,6 +83,109 @@ async function searchInspection(req, res) {
 
 }
 
+async function getAllInspections (req, res){
+  try {
+      const role = req.user.data.role;
+      let { propertyID, search, inspectionStatus, sortBy } = req.query;
+      let page = Number(req.query.page || 1);
+      let count = Number(req.query.count || 20);
+      let query = {};
+      let query2 = {};
+      if (propertyID) { query.propertyID = propertyID };
+      if (role == UserRoles.LANDLORD) { query.landlordID = req.user.data._id };
+      if (role == UserRoles.RENTER) { query["RenterDetails.id"] = req.user.data._id };
+      if(inspectionStatus){query.inspectionStatus = {$in : inspectionStatus.split(",")}}
+
+      let skip = Number(page - 1) * count;
+      if (search) {
+          query2.$or = [
+              { message: { $regex: search, $options: 'i' } },
+              { "RenterDetails.fullName": { $regex: search, $options: 'i' } },
+          ]
+      }
+      let field = "createdAt";
+      let order = "desc";
+      let sort_query = {};
+      if (sortBy) {
+          field = sortBy.split(' ')[0];
+          order = sortBy.split(' ')[1];
+      }
+      sort_query[field] = order == "desc" ? -1 : 1;
+      let pipeline = [
+          {
+              $match: query
+          },
+          {
+            $set : {
+              propertyID : {$toObjectId : "$propertyID"},
+            }
+          },
+          {
+            $lookup: {
+                from: "properties",
+                localField: "propertyID",
+                foreignField: "_id",
+                as: "property_details"
+            }
+        },
+        {
+            $unwind: {
+                path: "$property_details",
+                preserveNullAndEmptyArrays: true
+            }
+        },
+          {
+              $project: {
+                  RenterDetails : "$RenterDetails",
+                  inspectionTime : "$inspectionTime",
+                  inspectionDate : "$inspectionDate",
+                  message : "$message",
+                  inspectionApproved : "$inspectionApproved",
+                  inspectionStatus : "$inspectionStatus",
+                  propertyID : "$propertyID",
+                  landlordID : "$landlordID",
+                  id : "$id",
+                  fullDay : "$fullDay",
+              }
+          },
+          {
+              $match: query2
+          },
+          {
+              $facet: {
+                  pagination: [
+                      {
+                          $count: "total"
+                      },
+                      {
+                          $addFields: {
+                              page: Number(page)
+                          }
+                      }
+                  ],
+                  data: [
+                      {
+                          $sort: sort_query
+                      },
+                      {
+                          $skip: Number(skip)
+                      },
+                      {
+                          $limit: Number(count)
+                      },
+                  ],
+
+              }
+          }
+
+      ]
+      let get_inspections = await Inspection.aggregate(pipeline);
+      return sendResponse(res, get_inspections, "success", true, 200);
+  } catch (error) {
+      return sendResponse(res, {}, `${error}`, false, 500);
+  }
+}
+
 export {
   addInspection,
   getInsepction,
@@ -88,5 +193,6 @@ export {
   inspectionEdit,
   getAvailableDates,
   getInspectionRequests,
-  searchInspection
+  searchInspection,
+  getAllInspections
 };
