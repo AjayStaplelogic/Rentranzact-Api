8a5486ef4fc37275570ac364c84d575644cb9bb1
vijay <vijay@staplelogic.in>
2024-08-10 10:19:08 +0530

    created new rent application listing api

--- src/user/controllers/rentApplication.controller.mjs
+++ src/user/controllers/rentApplication.controller.mjs
@@ -2,6 +2,7 @@ import { sendResponse } from "../helpers/sendResponse.mjs";
 import { addRentApplicationService, rentApplicationsList, updateRentApplications, getRentApplicationsByUserID, getRentApplicationByID } from "../services/rentapplication.service.mjs"
 import { rentApplication } from "../models/rentApplication.model.mjs";
 import { identityVerifier } from "../helpers/identityVerifier.mjs";
+import { UserRoles } from "../enums/role.enums.mjs";
 
 async function addRentApplication(req, res) {
 
@@ -109,4 +110,143 @@ async function editRentApplication(req, res) {
 
 }
 
-export { addRentApplication, rentApplications, rentApplicationUpdate, getRentApplications, rentApplicationsByID, editRentApplication };
+async function getAllRentApplications(req, res) {
+  try {
+    let { search, applicationStatus, sortBy, propertyID } = req.query;
+    let page = Number(req.query.page || 1);
+    let count = Number(req.query.count || 20);
+    let skip = Number(page - 1) * count;
+    let query = {};
+    let query2 = {};
+    if (req?.user?.data?.role == UserRoles.RENTER) {
+      query.renterID = req?.user?.data?._id;
+    } else if (req?.user?.data?.role == UserRoles.LANDLORD) {
+      query.landlordID = req?.user?.data?._id;
+    }
+
+    if (applicationStatus) {
+      query.applicationStatus = { $in: applicationStatus.split(',') };
+    }
+
+    if (propertyID) { query.propertyID = propertyID; };
+
+    let field = "updatedAt";
+    let order = "desc";
+    let sort_query = {};
+    if (sortBy) {
+      field = sortBy.split(' ')[0];
+      order = sortBy.split(' ')[1];
+    }
+    sort_query[field] = order == "desc" ? -1 : 1;
+
+    if (search) {
+      query2.$or = [
+        { "property_info.propertyName": { "$regex": search, "$options": "i" } },
+        { "renter_info.fullName": { "$regex": search, "$options": "i" } },
+        { name: { "$regex": search, "$options": "i" } },
+        { employerName: { "$regex": search, "$options": "i" } },
+      ]
+    }
+
+    let pipeline = [
+      {
+        $match: query
+      },
+      {
+        $lookup: {
+          from: "users",
+          let: { renter_ID: { $toObjectId: "$renterID" } },
+          pipeline: [
+            {
+              $match: {
+                $expr: { $eq: ["$_id", "$$renter_ID"] }
+              }
+            },
+            {
+              $project: {
+                _id: 1,
+                fullName: 1, // Include fullName field from users collection
+                countryCode: 1,
+                phone: 1,
+                picture: 1
+
+              }
+            }
+          ],
+          as: "renter_info",
+        }
+      },
+      {
+        $unwind : {
+          path : "$renter_info",
+          preserveNullAndEmptyArrays: true
+        }
+      },
+      {
+        $lookup: {
+          from: "properties",
+          let: { propertyID: { $toObjectId: "$propertyID" } },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $eq: ["$_id", "$$propertyID"]
+                }
+              }
+            },
+            {
+              $project: {
+                _id: 1,
+                propertyName: 1,
+                images: "$images",
+                address: "$address"
+              }
+            }
+          ],
+          as: "property_info"
+        }
+      },
+      {
+        $unwind : {
+          path : "$property_info",
+          preserveNullAndEmptyArrays: true
+        }
+      },
+      {
+        $match: query2
+      },
+      {
+        $facet: {
+          pagination: [
+            {
+              $count: "total"
+            },
+            {
+              $addFields: {
+                page: Number(page)
+              }
+            }
+          ],
+          data: [
+            {
+              $sort: sort_query
+            },
+            {
+              $skip: Number(skip)
+            },
+            {
+              $limit: Number(count)
+            },
+          ]
+        }
+      }
+    ]
+    let data = await rentApplication.aggregate(pipeline);
+    return sendResponse(res, data, "Success", true, 200);
+
+  } catch (error) {
+    return sendResponse(res, {}, `${error}`, false, 500);
+  }
+
+}
+export { addRentApplication, rentApplications, rentApplicationUpdate, getRentApplications, rentApplicationsByID, editRentApplication, getAllRentApplications };
--- src/user/routes/rentApplication.route.mjs
+++ src/user/routes/rentApplication.route.mjs
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { addRentApplication, rentApplications, rentApplicationUpdate, getRentApplications, rentApplicationsByID, editRentApplication } from "../controllers/rentApplication.controller.mjs";
+import { addRentApplication, rentApplications, rentApplicationUpdate, getRentApplications, rentApplicationsByID, editRentApplication, getAllRentApplications } from "../controllers/rentApplication.controller.mjs";
 import multer from "multer";
 import { generateRandomFileName } from "../helpers/randomNameGenerator.mjs";
 import authorizer from "../middleware/authorizer.middleware.mjs";
@@ -28,5 +28,8 @@ router.post("/rentApplication/update-status",
 router.get("/rent-applications/:id", authorizer([UserRoles.LANDLORD, UserRoles.PROPERTY_MANAGER]), getRentApplications)
 
 router.put("/rentApplication",authorizer([UserRoles.RENTER]),editRentApplication)
+
+router.get('/rent-application/list', authorizer([UserRoles.RENTER, UserRoles.LANDLORD]), getAllRentApplications);
+
 
 export default router;
