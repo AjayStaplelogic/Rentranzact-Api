69c67a8deff5167ffae3723508bb41edc2d94839
vijay <vijay@staplelogic.in>
2024-08-12 15:02:13 +0530

    created get inspection by id API

--- src/user/controllers/inspection.controller.mjs
+++ src/user/controllers/inspection.controller.mjs
@@ -27,7 +27,7 @@ async function addInspection(req, res) {
   const renterID = req.user.data._id;
 
   const data = await createInspection(body, renterID);
-  
+
 
   sendResponse(res, data.data, data.message, data.status, data.statusCode);
 }
@@ -57,7 +57,7 @@ async function inspectionEdit(req, res) {
 }
 
 async function getInspectionRequests(req, res) {
- 
+
   const id = req.user.data._id;
   const role = req.user.data.role;
   const PropertyID = req.params.id;
@@ -69,104 +69,117 @@ async function getInspectionRequests(req, res) {
 }
 
 async function searchInspection(req, res) {
- 
+
   const id = req.user.data._id;
   const role = req.user.data.role;
-  const {search} = req.query;
-  const {status} = req.query;
+  const { search } = req.query;
+  const { status } = req.query;
 
 
   // console.log(id , role , search , status , "==parammmmsss")
 
-  const data = await searchInspectionService(id, role, search ,status);
+  const data = await searchInspectionService(id, role, search, status);
   sendResponse(res, data.data, data.message, data.status, data.statusCode);
 
 }
 
-async function getAllInspections (req, res){
+async function getAllInspections(req, res) {
   try {
-      const role = req.user.data.role;
-      let { propertyID, search, inspectionStatus, sortBy } = req.query;
-      let page = Number(req.query.page || 1);
-      let count = Number(req.query.count || 20);
-      let query = {};
-      let query2 = {};
-      if (propertyID) { query.propertyID = propertyID };
-      if (role == UserRoles.LANDLORD) { query.landlordID = req.user.data._id };
-      if (role == UserRoles.RENTER) { query["RenterDetails.id"] = req.user.data._id };
-      if(inspectionStatus){query.inspectionStatus = {$in : inspectionStatus.split(",")}}
+    const role = req.user.data.role;
+    let { propertyID, search, inspectionStatus, sortBy } = req.query;
+    let page = Number(req.query.page || 1);
+    let count = Number(req.query.count || 20);
+    let query = {};
+    let query2 = {};
+    if (propertyID) { query.propertyID = propertyID };
+    if (role == UserRoles.LANDLORD) { query.landlordID = req.user.data._id };
+    if (role == UserRoles.RENTER) { query["RenterDetails.id"] = req.user.data._id };
+    if (inspectionStatus) { query.inspectionStatus = { $in: inspectionStatus.split(",") } }
 
-      let skip = Number(page - 1) * count;
-      if (search) {
-          query2.$or = [
-              { message: { $regex: search, $options: 'i' } },
-              { "RenterDetails.fullName": { $regex: search, $options: 'i' } },
-          ]
-      }
-      let field = "createdAt";
-      let order = "desc";
-      let sort_query = {};
-      if (sortBy) {
-          field = sortBy.split(' ')[0];
-          order = sortBy.split(' ')[1];
-      }
-      sort_query[field] = order == "desc" ? -1 : 1;
-      let pipeline = [
-          {
-              $match: query
-          },
-          {
-              $project: {
-                  RenterDetails : "$RenterDetails",
-                  inspectionTime : "$inspectionTime",
-                  inspectionDate : "$inspectionDate",
-                  message : "$message",
-                  inspectionApproved : "$inspectionApproved",
-                  inspectionStatus : "$inspectionStatus",
-                  propertyID : "$propertyID",
-                  landlordID : "$landlordID",
-                  id : "$id",
-                  fullDay : "$fullDay",
-              }
-          },
-          {
-              $match: query2
-          },
-          {
-              $facet: {
-                  pagination: [
-                      {
-                          $count: "total"
-                      },
-                      {
-                          $addFields: {
-                              page: Number(page)
-                          }
-                      }
-                  ],
-                  data: [
-                      {
-                          $sort: sort_query
-                      },
-                      {
-                          $skip: Number(skip)
-                      },
-                      {
-                          $limit: Number(count)
-                      },
-                  ],
-
-              }
-          }
-
+    let skip = Number(page - 1) * count;
+    if (search) {
+      query2.$or = [
+        { message: { $regex: search, $options: 'i' } },
+        { "RenterDetails.fullName": { $regex: search, $options: 'i' } },
       ]
-      let get_inspections = await Inspection.aggregate(pipeline);
-      return sendResponse(res, get_inspections, "success", true, 200);
+    }
+    let field = "createdAt";
+    let order = "desc";
+    let sort_query = {};
+    if (sortBy) {
+      field = sortBy.split(' ')[0];
+      order = sortBy.split(' ')[1];
+    }
+    sort_query[field] = order == "desc" ? -1 : 1;
+    let pipeline = [
+      {
+        $match: query
+      },
+      {
+        $project: {
+          RenterDetails: "$RenterDetails",
+          inspectionTime: "$inspectionTime",
+          inspectionDate: "$inspectionDate",
+          message: "$message",
+          inspectionApproved: "$inspectionApproved",
+          inspectionStatus: "$inspectionStatus",
+          propertyID: "$propertyID",
+          landlordID: "$landlordID",
+          id: "$id",
+          fullDay: "$fullDay",
+        }
+      },
+      {
+        $match: query2
+      },
+      {
+        $facet: {
+          pagination: [
+            {
+              $count: "total"
+            },
+            {
+              $addFields: {
+                page: Number(page)
+              }
+            }
+          ],
+          data: [
+            {
+              $sort: sort_query
+            },
+            {
+              $skip: Number(skip)
+            },
+            {
+              $limit: Number(count)
+            },
+          ],
+
+        }
+      }
+
+    ]
+    let get_inspections = await Inspection.aggregate(pipeline);
+    return sendResponse(res, get_inspections, "success", true, 200);
   } catch (error) {
-      return sendResponse(res, {}, `${error}`, false, 500);
+    return sendResponse(res, {}, `${error}`, false, 500);
   }
 }
 
+async function getInspectionById(req, res) {
+  try {
+    let { id } = req.query;
+    let get_inspection = await Inspection.findById(id).lean().exec();
+    if (get_inspection) {
+      return sendResponse(res, get_inspection, "success", true, 200);
+    }
+    return sendResponse(res, {}, "Invalid Id", false, 400);
+
+  } catch (error) {
+    return sendResponse(res, {}, `${error}`, false, 500);
+  }
+}
 export {
   addInspection,
   getInsepction,
@@ -175,5 +188,6 @@ export {
   getAvailableDates,
   getInspectionRequests,
   searchInspection,
-  getAllInspections
+  getAllInspections,
+  getInspectionById
 };
