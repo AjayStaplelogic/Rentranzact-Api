5a07177f2533b966109d5c4d85f98165485c186d
vijay <vijay@staplelogic.in>
2024-08-14 12:02:10 +0530

    created rent transactions API

--- src/user/routes/transaction.route.mjs
+++ src/user/routes/transaction.route.mjs
@@ -2,11 +2,14 @@ import express from 'express'
 const router = express.Router();
 import authorizer from '../middleware/authorizer.middleware.mjs';
 import { UserRoles } from '../enums/role.enums.mjs';
-import { myTransaction , transactionById } from '../controllers/transaction.controller.mjs';
+import { myTransaction , transactionById, getAllRentTransactions } from '../controllers/transaction.controller.mjs';
 
 router.get('/my-transactions' , authorizer([UserRoles.RENTER , UserRoles.LANDLORD , UserRoles.PROPERTY_MANAGER]) ,  myTransaction);
 
-router.get('/transaction/:id' , authorizer([UserRoles.RENTER, UserRoles.LANDLORD]) , transactionById)
+router.get('/transaction/:id' , authorizer([UserRoles.RENTER, UserRoles.LANDLORD]) , transactionById);
+
+router.get('/transactions/rent' , authorizer([UserRoles.LANDLORD ]) ,  getAllRentTransactions);
+
 
 
 export default router;
