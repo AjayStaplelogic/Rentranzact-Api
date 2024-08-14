2f15681d3134cf3ba0bcff96055541945110bbb0
vijay <vijay@staplelogic.in>
2024-08-14 15:44:51 +0530

    created my renter history API

--- src/user/routes/rentingHistory.route.mjs
+++ src/user/routes/rentingHistory.route.mjs
@@ -1,12 +1,12 @@
 import express from 'express'
 const router = express.Router();
 import { UserRoles } from "../enums/role.enums.mjs"
-import { myRenters, viewMyRenter, getAllMyRenters } from '../controllers/myrenter.controller.mjs';
+import { myRenters, myRenterHistory, getAllMyRenters } from '../controllers/myrenter.controller.mjs';
 import authorizer from '../middleware/authorizer.middleware.mjs';
 
 router.get('/my-renters', authorizer([UserRoles.LANDLORD]), myRenters);
 router.get('/v2/my-renters', authorizer([UserRoles.LANDLORD]), getAllMyRenters);
-router.get('/my-renter', authorizer([UserRoles.LANDLORD]), viewMyRenter);
+router.get('/my-renter/history', authorizer([UserRoles.LANDLORD]), myRenterHistory);
 
 export default router;
 
