78b40987b20dd9035ed07a667e390772ed2fdff7
vijay <vijay@staplelogic.in>
2024-08-20 16:39:16 +0530

    create notification when rent application cancelled or withdraw

--- src/user/helpers/sendNotification.mjs
+++ src/user/helpers/sendNotification.mjs
@@ -1,43 +1,46 @@
 import admin from "../../server.js";
 async function sendNotification(user, type, title, body, metadata, role) {
+  try {
+    console.log(body, "----body in sendNotification")
+    const message = {
+      notification: {
+        title: title.toString(),
+        body: body.toString()
+      },
+      data: metadata,
+      token: user?.fcmToken
+    };
 
-  console.log(body, "----body in sendNotification")
-  const message = {
-    notification: {
-      title: title.toString(),
-      body: body.toString()
-    },
-    data: metadata,
-    token : user?.fcmToken
-  };
-
-  console.log(user.fcmToken ," ===============fcm token")
-
-
-  if (type === "single") {
-
-    admin.messaging().subscribeToTopic(message.token , role)
-    .then((response) => {
-      console.log("=====subscribe to topic" , response)
-    })
-    .catch((error) => {
-      console.log('error -----------------------' , error)
-    });
+    console.log(user.fcmToken, " ===============fcm token")
 
 
-    admin.messaging().send(message)
-      .then((response) => {
-         console.log('Notification sent:', response);
-      })
-      .catch((error) => {
-        console.error('Error sending notification:', error);
-      });
-  } else if (type === "multiple") {
+    if (type === "single") {
 
-    admin.messaging().sendEachForMulticast(message).then((respsone) => console.log("sent messages")).catch((error) => console.log("failed message"))
+      admin.messaging().subscribeToTopic(message.token, role)
+        .then((response) => {
+          console.log("=====subscribe to topic", response)
+        })
+        .catch((error) => {
+          console.log('error -----------------------', error)
+        });
 
+
+      admin.messaging().send(message)
+        .then((response) => {
+          console.log('Notification sent:', response);
+        })
+        .catch((error) => {
+          console.error('Error sending notification:', error);
+        });
+    } else if (type === "multiple") {
+
+      admin.messaging().sendEachForMulticast(message).then((respsone) => console.log("sent messages")).catch((error) => console.log("failed message"))
+
+    }
+
+  } catch (error) {
+    console.log(error)
   }
-
 }
 export default sendNotification;
 
