a86471d5fd5767885268ad8a4cc9121000516b37
ajay <ajay.staplelogic@gmail.com>
2024-08-21 19:11:48 +0530

    test

--- src/user/controllers/stripes.controller.mjs
+++ src/user/controllers/stripes.controller.mjs
@@ -1,5 +1,6 @@
 import { sendResponse } from "../helpers/sendResponse.mjs";
-import { addStripeTransaction, rechargeWallet } from "../services/strips.service.mjs";
+import { Property } from "../models/property.model.mjs";
+import { addStripeTransaction, rechargeWallet , addStripeTransactionForOld } from "../services/strips.service.mjs";
 async function stripe(req, res) {
 
     const { body } = req;
@@ -22,9 +23,30 @@ async function stripe(req, res) {
            
         } else {
 
-            const data = await addStripeTransaction(body , renterApplicationID);
 
-            sendResponse(res, data.data, data.message, data.status, data.statusCode);
+            const { propertyID } = body.data.object.metadata;
+
+
+            const property = await Property.findById(propertyID);
+
+
+            if(property.payment_count === 0) {
+
+                const data = await addStripeTransaction(body , renterApplicationID);
+
+                sendResponse(res, data.data, data.message, data.status, data.statusCode);
+            } else {
+
+                const data = await addStripeTransactionForOld(body , renterApplicationID);
+
+                sendResponse(res, data.data, data.message, data.status, data.statusCode);
+
+            }
+
+
+            
+
+            
 
           
 
