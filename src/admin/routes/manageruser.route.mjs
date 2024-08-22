9616931ed3c15285c3f2ad59a32d8b9d1f97ac6f
ajay <ajay.staplelogic@gmail.com>
2024-08-22 16:16:39 +0530

    test

--- src/admin/routes/manageruser.route.mjs
+++ src/admin/routes/manageruser.route.mjs
@@ -1,12 +1,13 @@
 import express from 'express'
 const router = express.Router();
-import { addUser , userList , user , deleteUser , searchUsers} from '../controllers/manageuser.controller.mjs';
+import { addUser , userList , user , deleteUser , searchUsers, updateStatus} from '../controllers/manageuser.controller.mjs';
 
 router.post('/addUser' ,addUser);
 router.post('/userList', userList);
 router.get('/user', searchUsers)
 router.get('/user/:id' , user )
-router.delete('/user/:id' , deleteUser)
+router.delete('/user/:id' , deleteUser);
+router.get('/update-status/:id' , updateStatus);
 
 export default router;
 
