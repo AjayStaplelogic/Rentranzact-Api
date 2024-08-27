1bb0a0ab5859c68e27253b96bca22b92da1520b2
vijay <vijay@staplelogic.in>
2024-08-27 16:13:22 +0530

    getEmployee added search and role filter

--- src/admin/services/manageeemployee.service.mjs
+++ src/admin/services/manageeemployee.service.mjs
@@ -2,78 +2,87 @@ import activityLog from "../helpers/activityLog.mjs";
 import { Admin } from "../models/admin.model.mjs";
 import pkg from "bcrypt";
 
-async function getEmployeeService(pageNo, pageSize) {
- 
+async function getEmployeeService(pageNo, pageSize, req) {
+
+  let { search, role } = req.query;
+  let query = {
+    role: { $ne: "superAdmin" }
+  };
+
+  if (search) {
+    query.$or = [
+      { fullName: { $regex: search, $options: 'i' } },
+      { email: { $regex: search, $options: 'i' } },
+    ]
+  }
+
+  if (role) { query.role = role };
   const skip = (pageNo - 1) * pageSize;
-    const data = await Admin.find({
-        role: { $ne: "superAdmin" }
-    }).skip(skip).limit(pageSize);
+  const data = await Admin.find(query).sort({ createdAt: -1 }).skip(skip).limit(pageSize);
 
-    const count = await Admin.countDocuments({
-      role: { $ne: "superAdmin" }
-  });
-    
-  
+  const count = await Admin.countDocuments(query);
+
+
+  return {
+    data: data,
+    message: `successfully fetched employee list`,
+    status: true,
+    statusCode: 200,
+    additionalData: { pageNo, pageSize, count }
+
+  };
+}
+
+async function addEmployeeService(body) {
+
+  const userExist = await Admin.findOne({ email: body.email });
+
+  if (userExist) {
     return {
-      data: data,
-      message: `successfully fetched employee list`,
-      status: true,
-      statusCode: 200,
-      additionalData : {pageNo , pageSize , count }
-      
+      data: [],
+      message: "email already exist ",
+      status: false,
+      statusCode: 409,
     };
   }
 
-  async function addEmployeeService(body) {
- 
-    const userExist = await Admin.findOne({ email: body.email});
+  let { password } = body;
 
-    if (userExist) {
-      return {
-        data: [],
-        message: "email already exist ",
-        status: false,
-        statusCode: 409,
-      };
-    }
-    
-    let { password } = body;
-  
-    const salt = parseInt(process.env.SALT);
-  
-    const hashedPassword = await new Promise((resolve, reject) => {
-      pkg.hash(password, salt, function (err, hash) {
-        if (err) {
-          reject(err); // Reject promise if hashing fails
-        } else {
-          resolve(hash); // Resolve promise with hashed password
-        }
-      });
+  const salt = parseInt(process.env.SALT);
+
+  const hashedPassword = await new Promise((resolve, reject) => {
+    pkg.hash(password, salt, function (err, hash) {
+      if (err) {
+        reject(err); // Reject promise if hashing fails
+      } else {
+        resolve(hash); // Resolve promise with hashed password
+      }
     });
-  
-    body.password = hashedPassword;
-    
-    const admin = new Admin(body);
-  
-    await admin.save();
+  });
 
-    await activityLog(admin._id , `created new employee ${admin.fullName}`)
-  
-    return {
-      data: admin,
-      message: "admin created successfully.",
-      statusCode: 201,
-    };
-  
-    return {
-      data: data,
-      message: `successfully fetched employee list`,
-      status: true,
-      statusCode: 201,
-    };
-  }
+  body.password = hashedPassword;
 
-  export {
-    getEmployeeService,
-    addEmployeeService
-  }
+  const admin = new Admin(body);
+
+  await admin.save();
+
+  await activityLog(admin._id, `created new employee ${admin.fullName}`)
+
+  return {
+    data: admin,
+    message: "admin created successfully.",
+    statusCode: 201,
+  };
+
+  return {
+    data: data,
+    message: `successfully fetched employee list`,
+    status: true,
+    statusCode: 201,
+  };
+}
+
+export {
+  getEmployeeService,
+  addEmployeeService
+}
