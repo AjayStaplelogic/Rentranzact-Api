5228cb40f050d3f22449fc6e60d83bc401871d38
vijay <vijay@staplelogic.in>
2024-09-16 13:03:54 +0530

    Added checks in my renters APIs for property manager

--- src/user/controllers/upload.controller.mjs
+++ src/user/controllers/upload.controller.mjs
@@ -1,5 +1,8 @@
 
+import multer from "multer";
 import { sendResponse } from "../helpers/sendResponse.mjs";
+import * as Multer from '../helpers/multer.mjs';
+import fs from "fs";
 
 export const uploadSingleImage = (req, res) => {
     try {
@@ -12,6 +15,60 @@ export const uploadSingleImage = (req, res) => {
 
     } catch (error) {
         return sendResponse(res, {}, `${error}`, false, 500);
+
+    }
+}
+
+export const uploadMultipleFiles = async (req, res) => {
+    try {
+        Multer.upload2.limits = {
+            fileSize: 10 * 1000000, // Converting 10 MB into bytes
+            files: 10,
+        };
+
+        let upload = Multer.upload2.array("media", 10);
+        upload(req, res, async function (err) {
+            if (err instanceof multer.MulterError) {
+                // A Multer error occurred when uploading.
+                return sendResponse(res, {}, err?.message, false, 400);
+
+            } else if (err) {
+                // An unknown error occurred when uploading.
+                return sendResponse(res, {}, err?.message, false, 500);
+            }
+
+            if (req.files && req.files.length) {
+                let res_arr = []
+                for await (let file of req.files) {
+                    let resObj = {
+                        ...file,
+                        fullPath: `${process.env.HOST_URL}${req?.body?.folder ?? "images"}/${file.filename}`
+                    }
+                    res_arr.push(resObj)
+                }
+                return sendResponse(res, res_arr, "success", true, 200);
+            }
+            return sendResponse(res, {}, "File not found", false, 400);
+        })
+    } catch (error) {
+        console.log(error, '=======error')
+        return sendResponse(res, {}, `${error}`, false, 500);
+
+    }
+}
+
+export const deleteFile = async (req, res) => {
+    try {
+        let { folder, filename } = req.query;
+        try {
+            fs.unlinkSync(`uploads/${folder ?? "images"}/${filename}`);
+        } catch (error) {
+            console.log(error);
+        }
+        return sendResponse(res, [], "success", true, 200);
+
+    } catch (error) {
+        return sendResponse(res, {}, error?.message ?? error, false, 400);
 
     }
 }
--- src/user/helpers/multer.mjs
+++ src/user/helpers/multer.mjs
@@ -2,37 +2,53 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 import { generateRandomFileName } from "../helpers/randomNameGenerator.mjs";
-
 const baseUploadPath = "uploads/";
 
-
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        console.log(req)
-        // console.log(JSON.stringify(req.body))
+  destination: function (req, file, cb) {
+    let { folder } = req.body;
+    folder = folder ?? "images"
+    const folderPath = path.join(baseUploadPath, folder);
 
-        // return
-
-        let {folder} = req.body;
-        console.log(folder, '====folder')
-        folder = folder?? "images"
-      const folderPath = path.join(baseUploadPath, folder);
-      console.log(folderPath, '====folderPath')
-  
-      // Create the directories if they don't exist
-      if (!fs.existsSync(folderPath)) {
-        fs.mkdirSync(folderPath,  { recursive: true });
-      }
+    // Create the directories if they don't exist
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath, { recursive: true });
+    }
     let destinationFolder = folderPath;
 
-      cb(null, destinationFolder);
-    },
-  
-    filename: function (req, file, cb) {
-      const randomFileName = generateRandomFileName(file);
-      console.log(randomFileName, '====randomFileName')
-      cb(null, randomFileName);
-    },
-  });
-  
+    cb(null, destinationFolder);
+  },
+
+  filename: function (req, file, cb) {
+    const randomFileName = generateRandomFileName(file);
+    cb(null, randomFileName);
+  },
+});
+
 export const upload = multer({ storage: storage });;
+
+
+const storage2 = multer.diskStorage({
+  destination: function (req, file, cb) {
+    let { folder } = req.body;
+    folder = folder ?? "images"
+    const folderPath = path.join(baseUploadPath, folder);
+
+    // Create the directories if they don't exist
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath, { recursive: true });
+    }
+    let destinationFolder = folderPath;
+    cb(null, destinationFolder);
+  },
+
+  filename: function (req, file, cb) {
+    const randomFileName = generateRandomFileName(file);
+    cb(null, randomFileName);
+  },
+
+});
+
+export const upload2 = multer({
+  storage: storage2
+});;
