9ca6739ee0b63180ab4f6fc61f4a8708243815d4
vijay <vijay@staplelogic.in>
2024-09-17 12:24:44 +0530

    added check in inspection post api and added renter id in inspection listing

--- src/user/routes/calender.route.mjs
+++ src/user/routes/calender.route.mjs
@@ -7,9 +7,9 @@ const router = express.Router();
 router.post("/calender", authorizer([UserRoles.RENTER, UserRoles.LANDLORD]) , calender);
 router.get("/calender", authorizer([UserRoles.RENTER, UserRoles.LANDLORD , UserRoles.PROPERTY_MANAGER]) , getCalender)
 router.post("/timeslot", authorizer([UserRoles.RENTER, UserRoles.LANDLORD]) , getTimeSlot)
-router.get("/calender/timeslot", authorizer([UserRoles.RENTER, UserRoles.LANDLORD]) , getCalenderTimeSlots)
-router.get("/calender/blocked-timeslot", authorizer([UserRoles.RENTER, UserRoles.LANDLORD, UserRoles.PROPERTY_MANAGER]) , getCalenderBlockedSlots)
-router.post("/calender/block/slot/multiple", authorizer([UserRoles.LANDLORD , UserRoles.PROPERTY_MANAGER]) , blockMultipleTimeSlots);
+router.get("/calender/timeslot", authorizer([UserRoles.RENTER, UserRoles.LANDLORD]) , getCalenderTimeSlots)// Inspections
+router.get("/calender/blocked-timeslot", authorizer([UserRoles.RENTER, UserRoles.LANDLORD, UserRoles.PROPERTY_MANAGER]) , getCalenderBlockedSlots) 
+router.post("/calender/block/slot/multiple", authorizer([UserRoles.LANDLORD , UserRoles.PROPERTY_MANAGER]) , blockMultipleTimeSlots);   
 
 
 export default router;
