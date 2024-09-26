0292bad817d8c95426bed35fdf58011ba7603b79
vijay <vijay@staplelogic.in>
2024-09-26 11:21:26 +0530

    changed recentTransaction data in landlord dashboard API

--- src/user/services/dashboard.service.mjs
+++ src/user/services/dashboard.service.mjs
@@ -19,7 +19,7 @@ async function getDashboardStats(user) {
         {
             $match: {
                 "landlordID": user._id,
-                "inspectionStatus" : InspectionStatus.INITIATED
+                "inspectionStatus": InspectionStatus.INITIATED
             }
         },
         { $sort: { createdAt: -1 } },
@@ -28,20 +28,52 @@ async function getDashboardStats(user) {
 
     const totalIncome = await Transaction.find({ landlordID: user._id, status: "succeeded" });
 
-    const recentTransaction = await Transaction.find({landlordID : user._id}).sort({createdAt : -1}).limit(3).select('amount property renter date')
+    // const recentTransaction = await Transaction.find({landlordID : user._id}).sort({createdAt : -1}).limit(3).select('amount property renter date')
 
-     let data = [{1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0, 10 : 0, 11 : 0, 12 : 0 }];
+    const recentTransaction = await Transaction.aggregate([
+        {
+            $match: {
+                landlordID: user._id
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                let: { userID: { $toObjectId: "$renterID" } }, // Convert propertyID to ObjectId
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: { $eq: ["$_id", "$$userID"] }, // Match ObjectId type
+                        },
+                    },
+                    { $project: { picture: 1 } }, // Project only the images array from properties
+                ],
+                as: "RenterDetails",
+            }
+        },
+        {
+            $sort: {
+                createdAt: -1
+            }
+        },
+        {
+            $limit: 3
+        }
+    ])
+
+
+    let data = [{ 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0 }];
 
     //  console.log(totalIncome)
 
     totalIncome.map((i) => {
         const date = moment.unix(i.date);
-        const month = date.month() + 1;      
+        const month = date.month() + 1;
 
         for (let obj of data) {
             if (obj?.hasOwnProperty(month)) {
                 obj[month] += i.amount;
-            } 
+            }
         }
     })
 
@@ -51,8 +83,8 @@ async function getDashboardStats(user) {
                 rented, vacant, maintenance, total
             },
             newestInspectionRequest: mostRecentInspection,
-            totalIncome : data,
-            recentTransaction :recentTransaction
+            totalIncome: data,
+            recentTransaction: recentTransaction
 
         },
         message: "dashboard stats",
@@ -77,7 +109,7 @@ async function getDashboardStatsPM(user) {
         {
             $match: {
                 "property_manager_id": user._id,
-                "inspectionStatus" : InspectionStatus.INITIATED
+                "inspectionStatus": InspectionStatus.INITIATED
             }
         },
         { $sort: { createdAt: -1 } },
@@ -86,20 +118,20 @@ async function getDashboardStatsPM(user) {
 
     const totalIncome = await Transaction.find({ pmID: user._id, status: "succeeded" });
 
-    const recentTransaction = await Transaction.find({pmID : user._id}).sort({createdAt : -1}).limit(3).select('amount property renter date')
+    const recentTransaction = await Transaction.find({ pmID: user._id }).sort({ createdAt: -1 }).limit(3).select('amount property renter date')
 
-     let data = [{1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0, 10 : 0, 11 : 0, 12 : 0 }];
+    let data = [{ 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0 }];
 
     //  console.log(totalIncome)
 
     totalIncome.map((i) => {
         const date = moment.unix(i.date);
-        const month = date.month() + 1;      
+        const month = date.month() + 1;
 
         for (let obj of data) {
             if (obj?.hasOwnProperty(month)) {
                 obj[month] += i.amount;
-            } 
+            }
         }
     })
 
@@ -109,8 +141,8 @@ async function getDashboardStatsPM(user) {
                 rented, vacant, maintenance, total
             },
             newestInspectionRequest: mostRecentInspection,
-            totalIncome : data,
-            recentTransaction :recentTransaction
+            totalIncome: data,
+            recentTransaction: recentTransaction
 
         },
         message: "dashboard stats",
@@ -121,4 +153,4 @@ async function getDashboardStatsPM(user) {
 }
 
 
-export { getDashboardStats  , getDashboardStatsPM};
+export { getDashboardStats, getDashboardStatsPM };
