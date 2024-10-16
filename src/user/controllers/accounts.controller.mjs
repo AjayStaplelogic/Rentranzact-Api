693dcea009b1106d84785760d668d57f54f80e80
vijay <vijay@staplelogic.in>
2024-10-16 18:44:06 +0530

    working on transfer module

--- src/user/controllers/accounts.controller.mjs
+++ src/user/controllers/accounts.controller.mjs
@@ -4,6 +4,7 @@ import * as StripeCommonServices from "../services/stripecommon.service.mjs"
 import mongoose from "mongoose";
 import ConnectedAccounts from "../models/connectedAccounts.model.mjs";
 import * as AccountServices from "../services/account.service.mjs";
+import Accounts from "../models/accounts.model.mjs";
 
 const ObjectId = mongoose.Types.ObjectId;
 
@@ -41,7 +42,25 @@ export const createConnectedAccount = async (req, res) => {
     }
 }
 
-export const getAllCards = async (req, res) => {
+export const getConnectedAccount = async (req, res) => {
+    try {
+        const get_account = await ConnectedAccounts.findOne({
+            user_id: req.user.data._id,
+            isDeleted: false
+        });
+
+        if (get_account) {
+            return sendResponse(res, get_account, "success", true, 200);
+        }
+
+        return sendResponse(res, {}, "Account not found", false, 404);
+    } catch (error) {
+        return sendResponse(res, null, `${error}`, false, 400);
+    }
+}
+
+
+export const getAllAccounts = async (req, res) => {
     try {
         let { search, sortBy } = req.query;
         let page = Number(req.query.page || 1);
@@ -51,7 +70,7 @@ export const getAllCards = async (req, res) => {
         let skip = Number(page - 1) * count;
         if (search) {
             query.$or = [
-                { name: { $regex: search, $options: 'i' } },
+                { account_holder_name: { $regex: search, $options: 'i' } },
             ]
         }
         let field = "createdAt";
@@ -62,7 +81,7 @@ export const getAllCards = async (req, res) => {
             order = sortBy.split(' ')[1];
         }
         sort_query[field] = order == "desc" ? -1 : 1;
-        query.user_id = { $eq: new ObjectId(req?.user?.data?._id) }
+        query.user_id = new ObjectId(req?.user?.data?._id);
 
         let pipeline = [
             {
@@ -73,15 +92,12 @@ export const getAllCards = async (req, res) => {
                     id: "$_id",
                     createdAt: "$createdAt",
                     updatedAt: "$updatedAt",
-                    gateway_type: "$gateway_type",
-                    user_id: "$user_id",
-                    name: "$name",
-                    last4: "$last4",
-                    exp_month: "$exp_month",
-                    exp_year: "$exp_year",
-                    // card_id: "$card_id",
-                    // customer_id: "$customer_id",
-                    isPrimary: "$isPrimary",
+                    account_holder_name: "$account_holder_name",
+                    bank_name: "$bank_name",
+                    country: "$country",
+                    currency: "$currency",
+                    status: "$status",
+                    isDeleted: "$isDeleted"
                 }
             },
             {
@@ -113,44 +129,9 @@ export const getAllCards = async (req, res) => {
 
         ]
 
-        let data = await Cards.aggregate(pipeline);
+        let data = await Accounts.aggregate(pipeline);
         return sendResponse(res, data, "success", true, 200);
     } catch (error) {
         return sendResponse(res, {}, `${error}`, false, 500);
-    }
-}
-
-export const deleteCard = async (req, res) => {
-    try {
-        let { id } = req.query;
-        if (!id) {
-            return sendResponse(res, {}, "Id required", false, 400);
-        }
-
-        let get_card = await Cards.findOne({ _id: id, user_id: req?.user?.data?._id });
-        if (get_card) {
-            if (get_card.gateway_type === "stripe") { // If the card from stripe, performing options on stripe
-
-                // Removing card from stripe
-                let delete_card = await StripeCommonServices.deleteCard({
-                    customer_id: get_card.customer_id,
-                    card_id: get_card.card_id
-                });
-
-                // IF the card successfully deleted from Stripe, then removing it from DB
-                if (delete_card && delete_card.deleted) {
-                    let remove_from_db = await Cards.findByIdAndDelete(get_card._id);
-                    if (remove_from_db) {
-                        return sendResponse(res, null, "Card deleted successfully", true, 200);
-                    }
-                    return sendResponse(res, null, "Failed to delete card", false, 500);
-                }
-                return sendResponse(res, null, "Failed to delete card from stripe", false, 500);
-            }
-            return sendResponse(res, null, "Invalid card type", false, 400);
-        }
-        return sendResponse(res, null, "Invalid Id", false, 400);
-    } catch (error) {
-        return sendResponse(res, null, `${error}`, false, 400);
     }
 }
