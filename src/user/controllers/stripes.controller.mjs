ee8544031c493683dbc808c5de60455edbe8f58c
vijay <vijay@staplelogic.in>
2024-11-08 14:36:09 +0530

    wallet withdrawl services

--- src/user/controllers/stripes.controller.mjs
+++ src/user/controllers/stripes.controller.mjs
@@ -65,8 +65,8 @@ async function stripe(req, res) {
 
 async function paystack(req, res) {
 
-    const testSecretKey = "sk_test_853a8821768ec289d7692eaadf8e920edf7afb70";
-    console.log(req.body, "-------------Pay Stack")
+    // const testSecretKey = "sk_test_853a8821768ec289d7692eaadf8e920edf7afb70";
+    // console.log(req.body, "-------------Pay Stack")
     let { body } = req;
     // body = JSON.parse(JSON.stringify(body));
     body.paymentMethod = "paystack"
@@ -75,52 +75,40 @@ async function paystack(req, res) {
         if (req?.body?.data?.reference?.includes("wallet")) {
             metadata = CommonHelpers.makePaystackMetaDataObjForNative(req?.body?.data?.reference);
         }
-        console.log(metadata, '======metadata111');
+        // console.log(metadata, '======metadata111');
         delete body.metadata;
         body.data.metadata = metadata;
-        console.log(body.metadata, '======body.metadata222');
+        // console.log(body.metadata, '======body.metadata222');
 
-        const hash = createHmac('sha512', testSecretKey).update(JSON.stringify(req.body)).digest('hex');
+        // const hash = createHmac('sha512', testSecretKey).update(JSON.stringify(req.body)).digest('hex');
         if (true) {
             const { wallet, renterApplicationID } = metadata;
 
             // console.log(wallet, "----------------------------> wallet")
-            console.log(req?.body?.data?.metadata?.custom_fields, "--------------> req.body.data.metadata?custom_fields:")
+            // console.log(req?.body?.data?.metadata?.custom_fields, "--------------> req.body.data.metadata?custom_fields:")
             // console.log(JSON.stringify(req?.body?.data?.metadata?.custom_fields), "--------------> JSON.stringify(req?.body?.data?.metadata?.custom_fields)")
-            console.log(wallet === "true", '======wallet === "true"');
+            // console.log(wallet === "true", '======wallet === "true"');
 
             if (wallet === "true") {
                 const data = await rechargeWallet(body);
                 return res.sendStatus(200);
-                sendResponse(res, data.data, data.message, data.status, data.statusCode);
             } else {
                 const { propertyID } = metadata;
                 const property = await Property.findById(propertyID);
                 if (property) {
-                    console.log("payment count ===>", property.payment_count)
+                    // console.log("payment count ===>", property.payment_count)
                     if (property.payment_count === 0) {
-                        const data = await addStripeTransaction(body, renterApplicationID);
+                         addStripeTransaction(body, renterApplicationID);
                         return res.sendStatus(200);
-                        sendResponse(res, data.data, data.message, data.status, data.statusCode);
                     } else {
-                        const data = await addStripeTransactionForOld(body, renterApplicationID);
+                        addStripeTransactionForOld(body, renterApplicationID);
                         return res.sendStatus(200);
-                        sendResponse(res, data.data, data.message, data.status, data.statusCode);
                     }
                 }
             }
-
-            return res.sendStatus(200);
-            return {
-                status: 200
-            }
-        } else {
-            console.log("------------> hash not working")
-            return {
-                status: 401
-            }
-        }
+        } 
     }
+    return res.sendStatus(200);
 }
 
 export { stripe, paystack };
