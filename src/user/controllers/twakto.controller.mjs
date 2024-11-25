394661a31dea9ff0ad98e2f9c2b830a2216a69c4
vijay <vijay@staplelogic.in>
2024-11-25 12:14:24 +0530

    checking twakto webhook

--- src/user/controllers/twakto.controller.mjs
+++ src/user/controllers/twakto.controller.mjs
@@ -1,8 +1,13 @@
 export const twawToWebhook = (req, res) => {
     try {
+
         console.log(req.headers, '====req.headers')
         console.log(req.body, '====req.body')
         console.log(req.query, '====req.query')
+        global.io.broadcast.emit('news', { hello: 'world Testing socket' });
+       const io = req.app.get('io');
+       io.broadcast.emit('attachedmanual', { hello: 'world Testing socket' });
+
     } catch (error) {
         console.error("Error while converting Twitch API data to Slack webhook payload:", error);
         return null;
