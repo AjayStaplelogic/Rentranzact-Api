1ea5cf4fe47a9162c009822ef6e248fc523000ae
vijay <vijay@staplelogic.in>
2025-01-02 15:09:05 +0530

    added some more data in transfer module

--- src/admin/routes/transfer.route.mjs
+++ src/admin/routes/transfer.route.mjs
@@ -5,11 +5,9 @@ import * as TransferController from "../controllers/transfers.controller.mjs"
 // Define routes for Faqs
 router.get('/transfers', TransferController.getAllTransfers);
 router.get('/transfer', TransferController.getTransferById);
-router.put('/transfer/update-status', TransferController.updateTransferStatus);
-router.put('/transfer/update-approval', TransferController.updateApprovalStatus);
-router.put('/transfer/initiate-approval', TransferController.updateInitiateApprovalStatus);
-
-
+router.put('/transfer/initiate-approval', TransferController.updateInitiateApprovalStatus); // Step 1: Initiate 
+router.put('/transfer/update-approval', TransferController.updateApprovalStatus); // Step 2: Update Approval
+router.put('/transfer/update-status', TransferController.updateTransferStatus);  // Step 3 : transfer
 
 export default router;
 
--- src/admin/validations/transfer.validation.mjs
+++ src/admin/validations/transfer.validation.mjs
@@ -2,12 +2,14 @@ import Joi from "joi";
 import { ETRANSFER_STATUS } from "../../user/enums/transfer.enums.mjs"
 
 export const updateTransferStatus = Joi.object().keys({
+    current_user_id: Joi.string().required(),
     id: Joi.string().required(),
     status: Joi.string().required().valid(ETRANSFER_STATUS.rejected, ETRANSFER_STATUS.transferred),
     description: Joi.string().optional().allow(""),
 });
 
 export const updateApprovalStatus = Joi.object().keys({
+    current_user_id: Joi.string().required(),
     id: Joi.string().required(),
     status: Joi.string().required().valid(ETRANSFER_STATUS.approvedByEmp, ETRANSFER_STATUS.rejectedByEmp),
     description: Joi.string().optional().allow(""),
@@ -15,6 +17,7 @@ export const updateApprovalStatus = Joi.object().keys({
 
 
 export const updateInitiateApprovalStatus = Joi.object().keys({
+    current_user_id: Joi.string().required(),
     id: Joi.string().required(),
     status: Joi.string().required().valid(ETRANSFER_STATUS.initiated, ETRANSFER_STATUS.initiateRejected),
     description: Joi.string().optional().allow(""),
