ab822709c3607955ca7caac41adf2ab941c9fb64
vijay <vijay@staplelogic.in>
2025-01-14 18:07:06 +0530

    fixing puppeeteer pdf issue

--- src/user/services/pdf.service.mjs
+++ src/user/services/pdf.service.mjs
@@ -2,7 +2,17 @@
 import puppeteer from "puppeteer";
 import fs from "fs";
 export const ConvertHtmlToPdf = async (htmlContent) => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+  args:[
+    "--disable-setuid-sandbox",
+    "--no-sandbox",
+    "--single-process",
+    "--no-zygote",
+  ],
+  executablePath:process.env.NODE_ENV === "production" ? 
+    process.env.PUPPETEER_EXECUTABLE_PATH
+    : puppeteer.executablePath(),
+});
     const page = await browser.newPage();
     await page.setContent(htmlContent);
     const pdfbuffer = await page.pdf({ format: 'A4' });
