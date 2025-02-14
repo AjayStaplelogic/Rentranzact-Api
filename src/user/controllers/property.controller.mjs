95d04d33dd8a9d354839c6865365d71ef4a7ae63
vijay <vijay@staplelogic.in>
2025-02-14 13:22:20 +0530

    removed logs from edit property api

--- src/user/controllers/property.controller.mjs
+++ src/user/controllers/property.controller.mjs
@@ -467,11 +467,6 @@ async function editProperty(req, res) {
     req.body.property_manager_id = property_manager_id ?? null;
     req.body.name = name;
     req.body.approval_status = ApprovalStatus.PENDING;
-    console.log(req.body, '==========req.body');
-    if(req.body.total_administrative_offices >= 0){
-      req.body.total_administrative_offices = Number(req.body.total_administrative_offices)
-    }
-
     const property = await Property.findByIdAndUpdate(id, req.body, { new: true });
     if (property) {
       if (property.property_manager_id && property.property_manager_id != get_property.property_manager_id && role === UserRoles.LANDLORD) {   // property have property manager then informing him via email
