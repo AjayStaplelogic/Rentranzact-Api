961a57c86732a90cd6b4631d4af48bbdf2859011
vijay <vijay@staplelogic.in>
2025-03-04 11:38:33 +0530

    created API to delete multiple files from s3

--- src/user/services/s3.service.mjs
+++ src/user/services/s3.service.mjs
@@ -70,3 +70,41 @@ export const deleteFileFromAws = async (fileName) => {
         return 'error';
     }
 };
+
+
+export const deleteMultipleFileFromAws = async (fileNames = []) => {
+    try {
+        // Configure the parameters for the S3 upload
+        const uploadParams = {
+            Bucket: BUCKET_NAME,
+            Delete: {
+                // "Objects": [
+                //     {
+                //         "Key": "HappyFace.jpg",
+                //         "VersionId": "2LWg7lQLnY41.maGB5Z6SWW.dcq0vx7b"
+                //     },
+                //     {
+                //         "Key": "HappyFace.jpg",
+                //         "VersionId": "yoz3HB.ZhCS_tKVEmIOr7qYyyAaZSKVd"
+                //     }
+                // ],
+                Objects: fileNames.map((item) => {
+                    console.log(item, '=====item')
+                    return { Key: item }
+                }),
+                "Quiet": false
+            }
+        };
+        // Upload the file to S3
+        const data = await s3Client.send(new DeleteObjectsCommand(uploadParams))
+        console.log(data, '======data');
+    } catch (err) {
+        console.error('Error ', err);
+        return 'error';
+    }
+};
+
+// console.log(await deleteMultipleFileFromAws([
+//     'property-media/a7baac7e-b939-4364-bc9e-768f4a0c6646/images/thumbnail-4cce75249930548807cb5052833a75de4dcfabdd.jpg',
+//     'property-media/a7baac7e-b939-4364-bc9e-768f4a0c6646/images/compressed-7debd8865eeef1cb2b9e5e162a3c757c6c9863b2.jpg'
+// ]))
