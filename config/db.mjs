5e110cc602424767098d0f4262314a989d1e374d
vijay <vijay@staplelogic.in>
2025-03-24 16:31:35 +0530

    testing github variables

--- config/db.mjs
+++ config/db.mjs
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 async function connectToMongoDB() {
   try {
+    console.log(process.env.TEST_SECRET, '===============process.env.TEST_SECRET');
+    console.log(process.env.TEST_VARIABLE, '===============process.env.TEST_VARIABLE');
     const client = await mongoose.connect(process.env.DATABASE_URL);
 
     console.log("Connected to MongoDB server");
