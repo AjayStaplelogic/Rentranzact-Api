1ed9d79001bc5c49d3b49f3d975082fa46f92de6
vijay <vijay@staplelogic.in>
2025-03-24 17:12:34 +0530

    testing github variables

--- src/user/services/s3.service.mjs
+++ src/user/services/s3.service.mjs
@@ -6,16 +6,8 @@ import {
 } from "@aws-sdk/client-s3"
 import fs from "fs"
 
-// const s3Client = new S3Client({
-//     region: process.env.AWS_REGION
-// });
-
 const s3Client = new S3Client({
-    region: process.env.AWS_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    }
+    region: process.env.AWS_REGION
 });
 
 
@@ -30,8 +22,6 @@ const BUCKET_NAME = process.env.S3_BUCKET_NAME;
  * @returns {string} keyName , File name from s3
  */
 export const uploadFile = async (filePath, keyName, contentType) => {
-    console.log(contentType, '==========contentType')
-    console.log(filePath, '==========filePath')
     const file = fs.createReadStream(filePath);
 
     const uploadParams = {
@@ -43,7 +33,6 @@ export const uploadFile = async (filePath, keyName, contentType) => {
     };
 
     const data = await s3Client.send(new PutObjectCommand(uploadParams));
-    console.log(data, '======data')
     if (data) {
         if (fs.existsSync(filePath)) {
             fs.unlink(filePath, (err) => {
@@ -95,18 +84,7 @@ export const deleteMultipleFileFromAws = async (fileNames = []) => {
         const uploadParams = {
             Bucket: BUCKET_NAME,
             Delete: {
-                // "Objects": [
-                //     {
-                //         "Key": "HappyFace.jpg",
-                //         "VersionId": "2LWg7lQLnY41.maGB5Z6SWW.dcq0vx7b"
-                //     },
-                //     {
-                //         "Key": "HappyFace.jpg",
-                //         "VersionId": "yoz3HB.ZhCS_tKVEmIOr7qYyyAaZSKVd"
-                //     }
-                // ],
                 Objects: fileNames.map((item) => {
-                    console.log(item, '=====item')
                     return { Key: item }
                 }),
                 "Quiet": false
@@ -114,7 +92,6 @@ export const deleteMultipleFileFromAws = async (fileNames = []) => {
         };
         // Upload the file to S3
         const data = await s3Client.send(new DeleteObjectsCommand(uploadParams))
-        console.log(data, '======data');
     } catch (err) {
         console.error('Error ', err);
         return 'error';
