1a3420f1dd3f5b736f13f0a905b4806a0e563156
vijay <vijay@staplelogic.in>
2025-04-04 13:00:25 +0530

    signup issue fixed

--- config/db.mjs
+++ config/db.mjs
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 async function connectToMongoDB() {
   try {
-    console.log(process.env.TEST_SECRET, '===============process.env.TEST_SECRET');
-    console.log(process.env.TEST_VARIABLE, '===============process.env.TEST_VARIABLE');
     const client = await mongoose.connect(process.env.DATABASE_URL);
-
     console.log("Connected to MongoDB server");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
