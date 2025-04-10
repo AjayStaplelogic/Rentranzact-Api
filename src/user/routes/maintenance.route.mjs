2f138f2e475ed84ff6425c3a15008b2c06945716
vijay <vijay@staplelogic.in>
2025-04-10 13:15:11 +0530

    maintiance request added checks and mulitiple remarks flow added

--- src/user/routes/maintenance.route.mjs
+++ src/user/routes/maintenance.route.mjs
@@ -1,20 +1,21 @@
 import express from 'express'
 import authorizer from '../middleware/authorizer.middleware.mjs';
 import { UserRoles } from '../enums/role.enums.mjs';
-import { addMaintenance , getMaintenanceRenter , resolveMaintenance , addRemark , cancelMaintenace, getMaintenanceDetails } from '../controllers/maintenance.controller.mjs';
+import { addMaintenance, getMaintenanceRenter, resolveMaintenance, addRemark, cancelMaintenace, getMaintenanceDetails } from '../controllers/maintenance.controller.mjs';
 const router = express.Router();
 
-router.post('/maintenance' , authorizer([UserRoles.RENTER]), addMaintenance);
+router.post('/maintenance', authorizer([UserRoles.RENTER]), addMaintenance);
 
-router.get('/maintenance' , authorizer([UserRoles.RENTER, UserRoles.LANDLORD, UserRoles.PROPERTY_MANAGER]), getMaintenanceRenter);
+router.get('/maintenance', authorizer([UserRoles.RENTER, UserRoles.LANDLORD, UserRoles.PROPERTY_MANAGER]), getMaintenanceRenter);
 
-router.get('/maintenance/:id' , authorizer([UserRoles.LANDLORD, UserRoles.PROPERTY_MANAGER]) , resolveMaintenance);
+router.get('/maintenance/:id', authorizer([UserRoles.LANDLORD, UserRoles.PROPERTY_MANAGER]), resolveMaintenance);
 
-router.get('/maintenance/cancel/:id', authorizer([UserRoles.RENTER]) , cancelMaintenace)
+router.get('/maintenance/cancel/:id', authorizer([UserRoles.RENTER]), cancelMaintenace)
 
-router.post('/maintenance/remark' ,  authorizer([UserRoles.LANDLORD, UserRoles.PROPERTY_MANAGER]) ,addRemark);
+router.post('/maintenance/remark', authorizer([UserRoles.LANDLORD, UserRoles.PROPERTY_MANAGER, UserRoles.RENTER]), addRemark);
 
-router.get('/maintenance-view' , authorizer([UserRoles.RENTER, UserRoles.LANDLORD, UserRoles.PROPERTY_MANAGER]), getMaintenanceDetails);
+router.get('/maintenance-view', authorizer([UserRoles.RENTER, UserRoles.LANDLORD, UserRoles.PROPERTY_MANAGER]), getMaintenanceDetails);
+
 
 
 export default router;
