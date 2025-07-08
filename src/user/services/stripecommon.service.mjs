1b1e1780987bcc34bb5db23b1991c1a3b34a5b87
vijay <vijay@staplelogic.in>
2025-07-08 11:09:00 +0530

    added US count when add stripe account

--- src/user/services/stripecommon.service.mjs
+++ src/user/services/stripecommon.service.mjs
@@ -109,8 +109,8 @@ export const deleteCard = async (options) => {
 export const createAccount = async (user) => {
     const account = await stripe.accounts.create({
         type: 'custom',
-        country: "NG",
-        default_currency: "usd",
+        // country: "NG",
+        country: "US",
         email: user.email,
         capabilities: {
             transfers: { requested: true, },
