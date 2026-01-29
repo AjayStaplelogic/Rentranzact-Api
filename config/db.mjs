2ebdf38159f04bbc76bb8e67df170ca7af0dc567
vijay <vijay@staplelogic.in>
2026-01-29 15:27:00 +0530

    added console

--- config/db.mjs
+++ config/db.mjs
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 async function connectToMongoDB() {
   try {
+    console.log("...conecting database")
     const client = await mongoose.connect(process.env.DATABASE_URL);
     console.log("Connected to MongoDB server");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
-    console.log(err?.message, '======message')
     throw err;
   }
 }
